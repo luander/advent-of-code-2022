4d5631246b8f73a088003d8bd510be89415b86c5
luander <iam@luander.net>
2022-12-04 21:55:11 +0100

    Day 4 part 2

--- day-4-camp-cleanup/src/main.rs
+++ day-4-camp-cleanup/src/main.rs
@@ -1,4 +1,4 @@
-fn calculate(input: &str) -> i32 {
+fn calculate(input: &str) -> (i32, i32) {
     let case: Vec<&str> = input.split(&['-', ','][..]).collect();
     let i1: i32 = case[0].parse().unwrap();
     let i2: i32 = case[1].parse().unwrap();
@@ -7,28 +7,35 @@ fn calculate(input: &str) -> i32 {
 
     // Not intersecting
     if i2 < j1 || i1 > j2 {
-        return 0;
+        return (0, 1);
     }
 
     // i is fully contained
     if i1 >= j1 && i2 <= j2 {
-        return 1;
+        return (1, 0);
     }
 
     // j is fully contained
     if j1 >= i1 && j2 <= i2 {
-        return 1;
+        return (1, 0);
     }
 
-    0
+    (0, 0)
 }
 
 fn main() {
-    let mut fully_contain = 0;
+    let mut part1 = 0;
+    let mut total_lines = 0;
+    let mut part2 = 0;
 
-    include_str!("../input.txt")
-        .lines()
-        .for_each(|line| fully_contain += calculate(line));
+    include_str!("../input.txt").lines().for_each(|line| {
+        let p1: i32;
+        let p2: i32;
+        (p1, p2) = calculate(line);
+        part1 += p1;
+        part2 += p2;
+        total_lines += 1;
+    });
 
-    println!("{}", fully_contain);
+    println!("{}, {}", part1, total_lines - part2);
 }
